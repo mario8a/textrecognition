7b25ea0f408c4656de53f5a54c8f34a9fb258a8e
Mario Ochoa <mario8ato@hotmail.com>
2025-01-18 17:19:29 -0600

    Gallery view completed

--- app/src/main/java/com/mario8a/textrecognition/views/GalleryView.kt
+++ app/src/main/java/com/mario8a/textrecognition/views/GalleryView.kt
@@ -1,12 +1,82 @@
 package com.mario8a.textrecognition.views
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.mario8a.textrecognition.R
 import com.mario8a.textrecognition.viewModel.ScannerViewModel
 
 @Composable
 fun GalleryView(
     viewModel: ScannerViewModel
 ) {
-    Text(text = "Gallery View")
+    val context = LocalContext.current
+    val clipboard = LocalClipboardManager.current
+    var image: Any? by remember { mutableStateOf(R.drawable.gallery) }
+
+    val photoPicker =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia()) {
+            if (it != null) {
+                image = it
+                viewModel.onRecognizedText(image, context = context)
+            } else {
+                viewModel.showToast(context, "No image selected")
+            }
+        }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                }
+                .padding(16.dp, 8.dp),
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        val scrollState = rememberScrollState()
+        Text(
+            text = viewModel.recognizedText,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(scrollState)
+                .clickable {
+                    clipboard.setText(AnnotatedString(viewModel.recognizedText))
+                    viewModel.showToast(context, "Text copied to clipboard")
+                }
+        )
+    }
 }
